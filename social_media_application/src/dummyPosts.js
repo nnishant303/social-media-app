6a7c5e659e8d490b6d3c294a8c8f98b5405e8519
nnishant303 <paliwalnishant0@gmail.com>
2026-07-16 11:18:17 +0530

    Initial things working

---
+++ social_media_application/src/dummyPosts.js
@@ -0,0 +1,18 @@
+const dummyPosts = [
+  {
+    id: 1,
+    title: "Went to Himalayas",
+    description: "A great experience there...",
+    hashtags: ["#Travel", "#Nature"],
+    views: 120
+  },
+  {
+    id: 2,
+    title: "Learning React",
+    description: "Today I learned useReducer.",
+    hashtags: ["#React", "#Coding"],
+    views: 85
+  }
+];
+
+export default dummyPosts;
